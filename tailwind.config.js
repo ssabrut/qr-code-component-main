ed26671e1ea35d5c5d2ca765b28e4b45665baff8
ssabrut <michael.gunawan2002@gmail.com>
2022-02-15 14:17:48 +0700

    Initial Commit:

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: ['./*.{html, js}'],
+  theme: {
+    extend: {
+      colors: {
+        'grayish-blue': 'hsl(220, 15%, 55%)',
+        'light-gray': 'hsl(212, 45%, 89%)',
+        'white': 'hsl(0, 0%, 100%)',
+        'dark-blue': 'hsl(218, 44%, 22%)'
+      },
+      fontFamily: {
+        'outfit': 'Outfit, sans-serif'
+      },
+      fontSize: {
+        'base': '15px'
+      },
+      height: {
+        '124': '500px'
+      }
+    },
+  },
+  plugins: [],
+}
